86dc3736783e0a7fe42e1f01921337a21207859f
emthasun <emilie.maier@yahoo.com>
2022-11-17 14:59:33 +0100

    Add svg

--- sketch.js
+++ sketch.js
@@ -1,55 +1,60 @@
 addEventListener("load", (event) => {
 
     var selected_letter = 0
-    var num = Math.floor(Math.random()*7)+1
+    var num = Math.floor(Math.random() * 7) + 1
     // random()
-    function random(direction){
+    function random(direction) {
         console.log('hey')
-        if(direction === 'up'){
-            num = Math.floor(Math.random()*7)+1
+        if (direction === 'up') {
+            num = Math.floor(Math.random() * 7) + 1
             selected_letter = 1
-            document.getElementsByClassName("letter_1")[0].style.backgroundImage = 'url("./images/up/img_' + num + '.png")'
+            document.getElementsByClassName("letter_1")[0].style.backgroundImage = 'url("./svg/up/img_' + num + '.svg")'
         }
-        if(direction === 'down'){
+        if (direction === 'down') {
             selected_letter = 2
-            document.getElementsByClassName("letter_2")[0].style.backgroundImage = 'url("./images/down/img_' + num + '.png")'
+            document.getElementsByClassName("letter_2")[0].style.backgroundImage = 'url("./svg/down/img_' + num + '.svg")'
         }
     }
 
 
 
 
-    document.getElementById("overlay").onclick = function(e) {
-        document.getElementById("overlay").classList.add("hidden");
-        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-            DeviceOrientationEvent.requestPermission().then((state) => {
-                if (state === 'granted') {
-                   window.addEventListener("deviceorientation", (event) => {
-                     document.getElementsByClassName("letter_1")[0].style.transform =  'scaleY('+map_range(event.beta, -90, 90, 0.000000001, 1.8)+')'
-                     document.getElementsByClassName("letter_2")[0].style.transform =  'scaleY('+map_range(event.beta, -90, 90, 1.8, 0.000000001)+')'
-                     document.getElementsByClassName("gradient_1")[0].style.transform =  'scaleY('+map_range(event.beta, -90, 90, 0.1, 1.9)+')'
-                     document.getElementsByClassName("gradient_2")[0].style.transform =  'scaleY('+map_range(event.beta, -90, 90, 1.9, 0.1)+')'
-                     if(event.beta < -75 && (selected_letter == 2 || selected_letter == 0)){
-                        random('up')
-                     }
-                     if(event.beta > 75  && (selected_letter == 1 || selected_letter == 0)){
-                        random('down')
-                     }
-                   }, true);
-                } else {
-                    document.getElementById("box").innerHTML = "access to device gyroscope denied";
-                }
+    document.getElementById("overlay").onclick = function (e) {
+        try {
+            
+            if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+                DeviceOrientationEvent.requestPermission().then((state) => {
+                    if (state === 'granted') {
+                        document.getElementById("overlay").classList.add("hidden");
+                        window.addEventListener("deviceorientation", (event) => {
+                            document.getElementsByClassName("letter_1")[0].style.transform = 'scaleX(200%) scaleY(' + map_range(event.beta, -90, 90, 0, 2) + ')'
+                            document.getElementsByClassName("letter_2")[0].style.transform = 'scaleX(200%) scaleY(' + map_range(event.beta, -90, 90, 2, 0) + ')'
+                            document.getElementsByClassName("gradient_1")[0].style.transform = 'scaleY(' + map_range(event.beta, -90, 90, 0, 2) + ')'
+                            document.getElementsByClassName("gradient_2")[0].style.transform = 'scaleY(' + map_range(event.beta, -90, 90, 2, 0) + ')'
+                            if (event.beta < -75 && (selected_letter == 2 || selected_letter == 0)) {
+                                random('up')
+                            }
+                            if (event.beta > 75 && (selected_letter == 1 || selected_letter == 0)) {
+                                random('down')
+                            }
+                        }, true);
+                    } else {
+                        document.getElementById("overlay").innerHTML = "access to device gyroscope denied";
+                    }
                 })
-            .catch(console.error);
-        } else {
-            window.addEventListener('deviceorientation', handleOrientation);
+            } else {
+                window.addEventListener('deviceorientation', handleOrientation);
+            }
+        } catch (err) {
+            document.getElementById("overlay").innerHTML = "gyroscope not supported";
         }
+
     };
 
 
 
-    function map_range(value, low1, high1, low2, high2){
-        return low2 + (high2 - low2)*(value - low1)/(high1 - low1)
+    function map_range(value, low1, high1, low2, high2) {
+        return low2 + (high2 - low2) * (value - low1) / (high1 - low1)
     }
 
 
